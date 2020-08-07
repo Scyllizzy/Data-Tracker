30cd11552e6e215cb72d8ea0aa57493df05eb84d
Scyllizzy <babytink2002@hotmail.com>
2020-08-07 12:31:44 -0700

    Created a model for creating a graph page

---
+++ DataTracker/Models/Graph.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataTracker.Models
+{
+    public class Graph
+    {
+        /// <summary>
+        /// Represents the type of graph to be visualized
+        /// </summary>
+        public enum Type
+        { 
+            line, bar, pie
+        }
+
+        /// <summary>
+        /// Represents the type of data the graph tracks
+        /// </summary>
+        public enum Data
+        {
+            Time, Money, Measurement, Custom
+        }
+
+        public Type GraphType { get; set; }
+
+        public Data GraphData { get; set; }
+    }
+}
