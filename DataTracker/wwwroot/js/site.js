6b5bfaf0a322eaeac2e2bae1fed41463365f69fd
Scyllizzy <babytink2002@hotmail.com>
2020-08-13 16:13:44 -0700

    Created a drop down for graph type

--- DataTracker/wwwroot/js/site.js
+++ DataTracker/wwwroot/js/site.js
@@ -1,4 +1,8 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+$(".dropdown-menu a").click(function () {
+    $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+    $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+});
+
