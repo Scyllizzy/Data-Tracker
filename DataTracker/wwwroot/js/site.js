6f3a670a599f7f807f7f74e4734ce72d0f87fac9
Scyllizzy <babytink2002@hotmail.com>
2020-08-13 17:11:13 -0700

    Added documentation

--- DataTracker/wwwroot/js/site.js
+++ DataTracker/wwwroot/js/site.js
@@ -1,6 +1,7 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+//Making the selected drop-down item visible
 $(".dropdown-menu a").click(function () {
     $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
     $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
